635fbfdf9eb875eb32d1056f1b6eae5de33b7451
zyc <501360128@qq.com>
2016-07-07 00:22:13 +0800

    start

--- modules/pages/page1/page.js
+++ modules/pages/page1/page.js
@@ -33,18 +33,18 @@ define(function (require, exports, module) {
         },
         selectImage: function () {
             var self = this;
-            this.dom.uploadFile.on("change", function () {
-                var file = this.files[0];
+            this.dom.uploadFile.on("change", function () { //当input发生改变的时候，
+                var file = this.files[0];//图片只能上传一张
 
-                if (!file.type.match('image.*')) {
+                if (!file.type.match('image.*')) {   //判断是否是图片类型
                     alert('请选择图片');
                     return false;
                 }
-                var reader = new FileReader();
-                reader.onload = function () {
-                    self.dom.img.attr('src',reader.result);
+                var reader = new FileReader();//new 一个新的file对象
+                reader.onload = function () {  //当图片加载完成后
+                    self.dom.img.attr('src',reader.result);//把当前转换的对象添加到img对象中
                 };
-                reader.readAsDataURL(file);
+                reader.readAsDataURL(file);//把读取到的图片编码成Data URL
 
             });
 
