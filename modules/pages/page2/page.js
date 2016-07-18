61e24e976d9534f1ac83d9a7b48a264b0095ab07
zyc <501360128@qq.com>
2016-07-18 21:58:21 +0800

    Merge branch 'master' of https://github.com/BQ-Team/web_module_tpl into qsc
    # Conflicts:
    #	app.js
    #	modules/pages/page1/page.js
    #	modules/pages/page2/page.html
    #	static/config/seaJS-config.js
    #	static/js/jquery.min.js

--- modules/pages/page2/page.js
+++ modules/pages/page2/page.js
@@ -4,6 +4,7 @@
 
 define(function (require, exports, module) {
     var baseModule = require('baseModule');
+    var util = require("util");
     module.exports = $.extend({}, baseModule, {
         html: $(__inline("./page.html")),
         title: "页面2",
@@ -16,11 +17,11 @@ define(function (require, exports, module) {
         },
         //每次切换进入到该组件 都会被执行
         load: function (params) {
-            logger.log(this.title, ' load');
+            util.logger.log(this.title, ' load');
         },
 
         initEvent: function () {
-            logger.log(this.title, 'ready , 事件初始化');
+            util.logger.log(this.title, 'ready , 事件初始化');
             this.html.on("click", ".topage1", function () {
                 showPage('#pages/page1', '123456789');
             });
